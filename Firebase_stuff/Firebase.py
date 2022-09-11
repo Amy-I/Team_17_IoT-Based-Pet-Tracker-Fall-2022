9408bcdc06f0bc8edd42c94459f783694926e45d
richardt713 <richardt713@tamu.edu>
2022-09-11 14:34:34 -0500

    updated firebase code

--- Firebase_stuff/Firebase.py
+++ Firebase_stuff/Firebase.py
@@ -1,11 +1,21 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
+import os
 
-cred = credentials.Certificate('firebase_sdk.json')
+cred = credentials.Certificate(r'C:\Users\richa\OneDrive\Documents\GitHub\Geofence\Firebase_stuff\firebase_sdk.json')
 
 firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://geofencingtest-342422-default-rtdb.firebaseio.com/'
 })
 
-ref = db,reference('/')
+ref = db.reference('/')
+ref.update({
+    'gpstest':{
+        'lat':'37',
+        'long':'30'
+    }
+})
+
+list = db.reference('/Trackers/123').get()
+print(list)
