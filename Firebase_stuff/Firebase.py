ad738ea8b707db7f5beee2268bfd62d74be7e47b
Amy Ideozu <a_ideozu@tamu.edu>
2022-10-06 12:32:13 -0500

    Added test to read isInGeofence value

--- Firebase_stuff/Firebase.py
+++ Firebase_stuff/Firebase.py
@@ -4,7 +4,7 @@ from firebase_admin import db
 import os
 
 cred = credentials.Certificate(r'C:\Users\richa\OneDrive\Documents\GitHub\Geofence\Firebase_stuff\firebase_sdk.json')
-
+#cred = credentials.Certificate(r'C:\Users\a_ide\StudioProjects\Geofence\Firebase_stuff\firebase_sdk.json')
 firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://geofencingtest-342422-default-rtdb.firebaseio.com/'
 })
@@ -27,4 +27,9 @@ tracker_ref.update({
     'latitude': 30.6532,
     'longitude': -963071
 })
+### End Test ###
+
+### Retrieving the Safe Area signal from database ###
+isInGeofence = tracker_ref.child('isInGeofence').get()
+print(isInGeofence)
 ### End Test ###
