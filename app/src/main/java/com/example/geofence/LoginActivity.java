d5100fc1aff418a849ebe61ccbc574cd6204d59e
Amy Ideozu <a_ideozu@tamu.edu>
2022-10-17 14:23:57 -0500

    Working on getting scalable screen size

--- app/src/main/java/com/example/geofence/LoginActivity.java
+++ app/src/main/java/com/example/geofence/LoginActivity.java
@@ -18,7 +18,6 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
     SharedPreferences sharedPreferences;
@@ -114,7 +113,7 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void goToLauncherPage(){
-        Intent goToLauncher = new Intent(this, LauncherActivity.class);
+        Intent goToLauncher = new Intent(this, LoginAndRegisterActivity.class);
         startActivity(goToLauncher);
     }
 }
